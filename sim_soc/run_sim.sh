62ec235308e91f7522b2af29bdc09e5a8717bab1
Core_kingdom <2972880695@qq.com>
2026-09-19 23:56:11 +0800

    run_sim.sh: 补上 -top glbl
    一步式 vcs 编译时 glbl 必须作为顶层参与 elaborate，否则 unisim 原语
    （IOBUF/OBUFDS/ISERDESE2/OSERDESE2/IDELAYE2）引用的 glbl.GSR/GTS
    解析失败，报 [XMRE] Cross-module reference resolution error。
    （与 ../work 的脚本保持一致）

--- sim_soc/run_sim.sh
+++ sim_soc/run_sim.sh
@@ -1,2 +1,3 @@
-vcs -full64 -sverilog -f arm926fpga.f -R -top tbench_top \
+# 一步式（VCS 直接编译+elaborate）：glbl 必须作为顶层，否则 unisim 的 glbl.GSR/GTS 解析失败
+vcs -full64 -sverilog -f arm926fpga.f -R -top tbench_top -top glbl \
     -l 1.log -P /home/synopsys/verdi/Verdi_O-2018.09-SP2/share/PLI/VCS/LINUXAMD64/novas.tab /home/synopsys/verdi/Verdi_O-2018.09-SP2/share/PLI/VCS/LINUXAMD64/pli.a
